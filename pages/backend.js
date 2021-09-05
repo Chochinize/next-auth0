cc779b94951fe678199255f44a214f1294022df5
chochiniz <chochinize@gmail.com>
2021-09-05 03:52:01 +0200

    next/

--- pages/backend.js
+++ pages/backend.js
@@ -80,7 +80,7 @@ const LogOn = ()=>{
      );
     }
 
-    export async function getServerSideProps(){
+    export async function getStaticProps(){
 
         const res = await fetch('http://localhost:3000/api/users/');
     const data = await res.json();
