36c06134c83bcc8b33fb7d8a6e6c7ddbdb87acd3
chochiniz <chochinize@gmail.com>
2021-09-05 05:33:22 +0200

    server  side

--- pages/backend.js
+++ pages/backend.js
@@ -80,7 +80,7 @@ const LogOn = ()=>{
      );
     }
 
-    export async function getStaticProps(){
+    export async function getServerSideProps(){
 
         
         const res = await fetch('http://localhost:3000/api/users/');
