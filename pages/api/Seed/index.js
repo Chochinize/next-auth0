49abcfa500565fa405322677c0e6cbee3e5bf7f6
chochiniz <chochinize@gmail.com>
2021-09-16 01:13:00 +0200

    reffering

--- pages/api/Seed/index.js
+++ pages/api/Seed/index.js
@@ -73,41 +73,41 @@ export default handler
 
 
 
-(async () => {
+// (async () => {
   
 
-    const MONGO_URI = process.env.NEXT_PUBLIC_TEST
-    mongoose.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() => console.log('Connection to DB established!'))
-      .catch(err => console.log(`[ERROR] Connection failed => ${err}`))
+//     const MONGO_URI = process.env.NEXT_PUBLIC_TEST
+//     mongoose.connect(MONGO_URI, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       })
+//       .then(() => console.log('Connection to DB established!'))
+//       .catch(err => console.log(`[ERROR] Connection failed => ${err}`))
 
 
 
-    const pizzaPromises = Array(15)
-      .fill(null)
-      .map(() => {
-        const pizzaData = {
-          name: faker.address.cityName(),
-          description: faker.commerce.description(),
-          price: faker.commerce.price(7.99, 13.80),
-          image: faker.image.food()
-        }
-        console.log(`Pizza ${pizzaData.name} has been created`)
-        return Pizz.create(pizzaData)
-      })
-    try {
-      await Promise.all(pizzaPromises)
-      console.log('==================================')
-      console.log('All 15 pizzas have been stored into the DB')
-      console.log('==================================')
-    } catch (err) {
-      console.log(err)
-    }
-    mongoose.connection.close()
-  })()
+//     const pizzaPromises = Array(15)
+//       .fill(null)
+//       .map(() => {
+//         const pizzaData = {
+//           name: faker.address.cityName(),
+//           description: faker.commerce.description(),
+//           price: faker.commerce.price(7.99, 13.80),
+//           image: faker.image.food()
+//         }
+//         console.log(`Pizza ${pizzaData.name} has been created`)
+//         return Pizz.create(pizzaData)
+//       })
+//     try {
+//       await Promise.all(pizzaPromises)
+//       console.log('==================================')
+//       console.log('All 15 pizzas have been stored into the DB')
+//       console.log('==================================')
+//     } catch (err) {
+//       console.log(err)
+//     }
+//     mongoose.connection.close()
+//   })()
 
 
 
