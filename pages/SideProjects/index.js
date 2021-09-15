ec3d3163f53c7efdf25f341593fad4b681889f4a
chochiniz <chochinize@gmail.com>
2021-09-16 01:35:19 +0200

    addpages

--- pages/SideProjects/index.js
+++ pages/SideProjects/index.js
@@ -14,7 +14,7 @@ import dbConnect from '../../utilis/dbConnection'
 
 
 const fetcher = (url)=> fetch(url).then((res)=>  res.json())
-const API = 'http://localhost:3000/api/Pizza-Store/'
+const API = process.env.PIZZA_ADDRESS1
 
 
 
